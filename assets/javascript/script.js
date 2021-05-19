d57e51283eddcb60e3ac38fdc23faf0c409d4d83
spreston <sam.preston11@gmail.com>
2021-05-19 17:49:05 -0500

    Added 1st version of pseudo-code for javascript functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,41 @@
+// Define variable array to store highscores
+
+// Append 'main' element to display start screen
+    // Include header
+    // Include start instructions as a paragraph
+    // Include 'start quiz' button
+        // When 'start quiz' button is pressed, clear the current display for main and begin the quiz function
+
+// ---------------------------------- Quiz Function ------------------------------------------------------------ //
+// Declare local variables - score keeper, etc
+
+// Begin a countdown timer as soon as quiz begins - display countdown text to the .time class
+    // If timer reaches 0, display end screen
+
+// First question - append to .main class
+    // Display question as header
+    // Display 4 buttons, each with a different answer on text
+    // Establish variable for the correct answer
+    // If correct answer is selected, current score increases & 'Correct!' is displayed to screen & diplay 'next question' button
+    // If incorrect answer is selected, current score is not affected & 'Wrong!' is displayed to screen & display 'next question' button
+    // When 'next question' button is pressed, current question is removed and next question is appended to the .main
+
+// Second question
+    // Repeat first question just with different text and selections
+
+// Third question
+    // Repeat
+
+// Fourth question
+    // Repeat
+
+// End Screen
+    // Display 'All Done!' as title
+    // Display score
+    // Display text box to enter initials
+    // Display button to 'submit' - on submit, clear current screen and diplay highscore screen
+
+// Display highscore
+    // List of high scores in order from highest to lowest
+    // Display return button - when pressed, returns to welcome message
+    // Display clear score button - when pressed, clear high score data
